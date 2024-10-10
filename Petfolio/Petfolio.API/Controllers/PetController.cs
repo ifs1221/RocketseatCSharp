80325c8f93c03e3cef37a81e4ba9530a776f7677
ifs1221 <israel.felix99@gmail.com>
2024-10-09 21:18:45 -0300

    adding communication classes and application use case

---
+++ Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.Register;
+using Petfolio.Communication.Requests;
+
+namespace Petfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(RequestRegisterPetJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody] RequestRegisterPetJson request)
+        {   
+            var response = new RegisterPetUseCase().Execute(request);
+            return Created(string.Empty, response);
+        }
+    }
+}
