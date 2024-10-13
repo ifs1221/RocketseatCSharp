5fffb6d6b5a2db0425a3c9f0d6ced9e7861aed18
ifs1221 <israel.felix99@gmail.com>
2024-10-13 15:13:05 -0300

    creating CashFlow solution

---
+++ CashFlow/src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson expense)
+    {
+        var response = new RegisterExpenseUseCase().Execute(expense);
+
+        return Created(string.Empty, response);
+    }
+
+}
